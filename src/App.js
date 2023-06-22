2be44fb7a1ae0afe6d806817d41da585ba264e8c
GianlorenzoDercole <johndercole01@gmail.com>
2023-06-22 15:12:24 -0700

    add nav bar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-
 import './App.css';
 import Recipe from './Recipe'
-
+import logo from './images/logo.png'
 function App() {
   return (
     <>
+      <nav className="navBar">
+        <img src={logo} className="logo"/>
+        <h3 className="title">React Recipes</h3>
+      </nav>
+      <div className="recipes">
+        < Recipe />
+      </div>
 
-      < Recipe />
     </>
   );
 }
-
 export default App;
